d792d36f3b589bd3e7f3e4bef2c0ddc91ca4f903
PCPCRACK <127131700+PCPCRACK@users.noreply.github.com>
2023-03-14 09:16:43 -0500

    Delete #Punto 6.py

--- #Punto 6.py
+++
@@ -1,5 +0,0 @@
-letra = input("Ingrese una letra: ")
-if letra.lower() in ['a', 'e', 'i', 'o', 'u']:
-    print("La letra ingresada es una vocal")
-else:
-    print("La letra ingresada es una consonante")
